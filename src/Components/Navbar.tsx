2d540ec92518ec1d06fbb0956d442afd93982f62
Lucas <arrizabalucas@gmail.com>
2023-05-23 09:17:12 +0200

    Remove navbar (useless)

--- src/Components/Navbar.tsx
+++
@@ -1,9 +0,0 @@
-import { NavLink } from "react-router-dom";
-
-export default function Navbar() {
-  return (
-    <nav className="w-full h-fit flex flex-row justify-between border-b-2 items-center bg-black/30">
-      <NavLink to="/" className="cursor-default"></NavLink>
-    </nav>
-  );
-}
